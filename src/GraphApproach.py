cbc96f0c76e0495b2aa498c1c8b22ae8f911ec6b
ocuadrosl <ocuadrosl@gmail.com>
2019-07-05 10:47:34 -0300

    # WARNING: head commit changed in the meantime
    graph

--- src/GraphApproach.py
+++ src/GraphApproach.py
@@ -15,92 +15,89 @@ class GraphApproach:
     
     '''
     @image a RGB image
+    @return edges and angles
     '''
 
-    def getEdges(self, image):
+    def getGradients(self, image):
         imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
               
         blur = cv2.GaussianBlur(imageGray, (3, 3), 0)
               
         binary = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
         
-        #gx, gy  = np.gradient(blur)
+        # gx, gy  = np.gradient(blur)
         
-        gx = cv2.Sobel(blur,cv2.CV_64F,1,0) 
-        gy = cv2.Sobel(blur,cv2.CV_64F,0,1)
+        gx = cv2.Sobel(blur, cv2.CV_64F, 1, 0) 
+        gy = cv2.Sobel(blur, cv2.CV_64F, 0, 1)
+               
+        angles = np.arctan2(gy, gx) * (180 / np.pi)
         
-        
-        angle = np.arctan2(gy, gx) * (180 / np.pi)
-        
-        h,w = imageGray.shape
-        x, y = np.mgrid[0:h, 0:w]
-        #plt.quiver(x, y, gx, gy, units='x', pivot='tip', width=0.09,   scale=1 / 0.01)
+        # h,w = imageGray.shape
+        # x, y = np.mgrid[0:h, 0:w]
+        # plt.quiver(x, y, gx, gy, units='x', pivot='tip', width=0.09,   scale=1 / 0.01)
           
-        plt.imshow(angle, cmap='hot')
-        plt.show()
+        # plt.imshow(angles, cmap='hot')
+        # plt.show()
         
         blur[np.where(binary == 255)] = 255  # aply threshold to blur
         
         edges = cv2.Canny(blur, 0, 255)
         
-        edges = np.matrix(edges, np.int32)
+        edges = np.matrix(edges, dtype=np.int64)
     
+        #plt.imshow(edges, cmap='hot')
+        #plt.show()
         
+        return edges, angles
     
-        # plt.imshow(edges, cmap='hot')
-        # plt.show()
-        
-        return edges
-    
-    def createGraph(self, edges, minRadius=1, maxRadius=10):
-        
+    def createGraph(self, edges, angles , minRadius=1, maxRadius=5):
+                
         height, width = edges.shape
         
+        vsIndex = []
         vertexLabel = 0
+        
         for h in range(0, height):
             for w in range(0, width):
                 if edges[h, w] == 255:
                     edges[h, w] = vertexLabel
+                    vsIndex.append((h, w)) 
                     vertexLabel += 1
+                    
                 else:
                     edges[h, w] = -1
         
-        # plt.imshow(edges, cmap='hot')
-        # plt.show() 
-        # print(VertexLabel)  
         
-        verticesNo = vertexLabel             
+        #print(vertexLabel)
+        #plt.imshow(edges, cmap='hot')
+        #plt.show() 
+          
         
-        vsIndex = [None] * verticesNo  # to store the correesponding image index
+        # verticesNo = vertexLabel             
+        
+        # vsIndex = [None] * verticesNo  # to store the correesponding image index
         tupleList = []  # store edges and weights
                
         # all pixels 
         for h in range(0, height):
             for w in range(0 , width):
-                                
-                if edges[h, w] == -1:  # is not an edge
-                                                          
-                    vLabelOrg = None
-                    for hM in range(h - maxRadius, h + maxRadius):
-                        for wM in range(w - maxRadius, w + maxRadius):
+                               
+                if edges[h, w] >= 0:  # is an edge
+                                                        
+                    vLabelOrg = edges[h, w]
+                    # vsIndex[vLabelOrg] = (h, w)
+                    
+                    for hD in range(h - maxRadius, h + maxRadius):
+                        for wD in range(w - maxRadius, w + maxRadius):
                                                    
                             try:  # avoid to access out of dimentions 
-                                if edges[hM, wM] >= 0:  # is an edge
-                                    vsIndex[edges[hM, wM]] = (hM, wM)
-                                     
-                                    if vLabelOrg is None:
-                                        vLabelOrg = edges[hM, wM]
-                                        hOrg, wOrg = hM, wM
-                                        continue   
-                                    
-                                    vLabelDest = edges[hM, wM];
-                                    
-                                    #print(vLabelOrg, vLabelDest)
-                                                                                             
-                                    weight = self.laplaceWeight((hOrg, wOrg), (hM, wM), (h, w), 0.001, minRadius, maxRadius)
+                                
+                                vLabelDest = edges[hD, wD]
+                                if vLabelDest >= 0 and vLabelDest != vLabelOrg:  # is an edge
+                                                                                                                                                                 
+                                    weight = self.laplaceWeight((h, w), (hD, wD), angles[h, w], angles[hD, wD], 1)
                                     # print(weight)
-                                    
-                                    tupleList.append((vLabelOrg, vLabelDest, weight))                                    
+                                    tupleList.append([vLabelOrg, vLabelDest, weight])                                    
                                     
                             except:
                                 pass        
@@ -114,9 +111,11 @@ class GraphApproach:
         
         print('Creating graph [OK]')
                 
+                
         membership = graph.community_fastgreedy(weights=graph.es["weight"]).as_clustering().membership
-                       
-        #membership = graph.community_multilevel(weights=graph.es["weight"]).membership 
+        
+                             
+        # membership = graph.community_multilevel(weights=graph.es["weight"]).membership 
         # membership = graph.community_label_propagation(weights=graph.es["weights"])   
         
         print('Clutering [OK]')
@@ -134,24 +133,20 @@ class GraphApproach:
         plt.imshow(clusters, cmap='jet')
         plt.show()
     
-    def laplaceWeight(self, p1, p2, pC, b, minRadius, maxRadius):
-                  
-                
-        dist1ToCent = distance.euclidean(p1, pC)
-        dist2ToCent = distance.euclidean(p2, pC)
-        dist1to2 = distance.euclidean(p1, p2)
+    def laplaceWeight(self, pOrg, pDest, angleOrg , angleDest, b):
+                           
+        fOrg = pOrg[0], pOrg[1]  # , angleOrg#, distOrgToCent
+        fDest = pDest[0], pDest[1]  # , angleDest#, distDestToCent   
         
-        diffToCent = np.abs(dist1ToCent - dist2ToCent) 
+        # print(distance.sqeuclidean(fOrg,fDest))
         
-        relation = diffToCent/dist1to2  if dist1to2 > 0 else diffToCent / 0.001
+        diff = distance.euclidean(pOrg, pDest)
+       
+        # print(diff, (1 / (2 * b)) * np.exp(-1 * ( diff / b)))
         
-        #relation  = np.abs(dist1to2-diffToCent) 
-        
-        # angle = np.degrees(np.arctan2(p2[1] - pC[1], p2[0] - pC[0]) - np.arctan2(p1[1] - pC[1], p1[0] - pC[0]))                
-               
-        # relation = distDiff / angle if angle > 0 else distDiff / 0.0001
-                      
-        return (1 / (2 * b)) * np.exp(-1 * (relation / b))
+        return np.exp(-1 * diff)
+                            
+        #return (1 / (2 * b)) * np.exp(-1 * (diff / b))
     
     def cosine(self, vector1, vector2):
         norm1 = np.linalg.norm(vector1)
@@ -188,7 +183,7 @@ class GraphApproach:
             return 0.0
     
     def process(self, rgbImage):
-        edges = self.getEdges(rgbImage)
-        self.createGraph(edges)  
+        edges, angles = self.getGradients(rgbImage)
+        self.createGraph(edges, angles)  
         print('OK')  
     
